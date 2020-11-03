9923c217efee4ceda018bc6809f5092e9035aadb
Rohit Kumar <52652090+Rohitkumar15697@users.noreply.github.com>
2020-11-03 13:00:15 +0530

    Update 1st video-captured_pics_saved.py

--- video-captured_pics_saved/1st video-captured_pics_saved.py
+++ video-captured_pics_saved/1st video-captured_pics_saved.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import os
 
 cap=cv2.VideoCapture(0)
 a=0
@@ -8,7 +7,6 @@ a=0
 #using loop because capturing frame by frame
 while(True):
         
-        path="F:\Study\Python\OpenCV\start\Video opencv programs\pics"
         ret,frame=cap.read()     #capture frame by frame
         
         #convert captured frames to gray  
@@ -17,7 +15,7 @@ while(True):
         #show gray frames
         cv2.imshow('FRAME',gray)
         #saving all frames with different name heloo1,2,3,4........jpg 
-        cv2.imwrite(os.path.join(path,'heloo'+str(a)+'.jpg'),gray)
+        cv2.imwrite("pics/heloo"+str(a)+".jpg",gray)
         a+=1
 
         if cv2.waitKey(1) & 0xFF==ord('q'):
